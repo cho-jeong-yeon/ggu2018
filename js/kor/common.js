53bbb780341ac19de2bb21742903b5e596e668bf
조정연 <joetaiji@plani.co.kr>
2018-07-27 10:41:23 +0900

    kor html 스크립트 common.js로 이동

--- js/kor/common.js
+++ js/kor/common.js
@@ -2,41 +2,74 @@
 $(document).ready(function(){
 
 
-//팝업
-$('#popup ul').bxSlider({
-	auto			: true,
-	autoHover		: true,
-	speed			: 1000,
-	pause			: 5000,
-	mode			: 'vertical',
-	responsive		: true,
-	pager			: true,
-	autoControls	: false,
-	controls		: false
-});
-$('#popup .close').click(function(){
-  $('#popup').addClass('close');
-  return false;
-});
+	//팝업
+	$('#popup ul').bxSlider({
+		auto			: true,
+		autoHover		: true,
+		speed			: 1000,
+		pause			: 5000,
+		mode			: 'vertical',
+		responsive		: true,
+		pager			: true,
+		autoControls	: false,
+		controls		: false
+	});
+	$('#popup .close').click(function(){
+	  $('#popup').addClass('close');
+	  return false;
+	});
 
 
 
-//대메뉴
-$('#gnb').on('mouseenter focusin', function(){
-	$(this).parents('#header').addClass('active');
-});
-$('#header').on('mouseleave', function(){
-  $(this).removeClass('active');
-	$('#gnb .depth1').removeClass('active');
-	$('#gnb .depth2 > li').removeClass('active');
-});
+	//대메뉴
+	$('#gnb').on('mouseenter focusin', function(){
+		$(this).parents('#header').addClass('active');
+	});
+	$('#header').on('mouseleave', function(){
+	  $(this).removeClass('active');
+		$('#gnb .depth1').removeClass('active');
+		$('#gnb .depth2 > li').removeClass('active');
+	});
 
-$('#gnb .depth1').on('mouseenter focusin',function(){
-	$(this).addClass('active').siblings().removeClass('active');
-});
-$('#gnb .depth2 > li').on('mouseenter focusin', function(){
-	$(this).addClass('active').siblings().removeClass('active');
-});
+	$('#gnb .depth1').on('mouseenter focusin',function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+	$('#gnb .depth2 > li').on('mouseenter focusin', function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+
+/* 퀵메뉴 */
+	$('#quick .menu').find('a').on('mouseenter focusin', function()
+	{
+		$(this).parent('li').siblings('li').removeClass('active');
+		$(this).parent('li').addClass('active');
+	});
+
+	$('#quick .menu').find('a').on('click', function()
+	{
+		$(this).parents('#quick').addClass('active');
+
+		return false;
+	});
+
+	$('#quick .banner').find('a').on('mouseenter focusin', function()
+	{
+		$(this).parent('li').siblings('li').removeClass('active');
+		$(this).parent('li').addClass('active');
+	});
+
+	$('#quick .link').find('a').on('mouseenter focusin', function()
+	{
+		$(this).siblings('a').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('#quick .group').find('.close').on('click', function()
+	{
+		$(this).parents('#quick').removeClass('active');
+
+		return false;
+	});
 
 
 
