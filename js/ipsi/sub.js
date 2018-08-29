d99d598c2bd9e8418ebf584eccc032f2cb6216cb
조정연 <joetaiji@plani.co.kr>
2018-08-29 14:13:47 +0900

    메인, 입시메인수정

--- js/ipsi/sub.js
+++ js/ipsi/sub.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function(){
 
-      
+
+});
+
+$(window).on("load scroll", function(){
+
+	var fixItem = $(".cont-top");
+  var baseline = $(".cont-wrap");
+  if(fixItem.offset().top <= $(window).scrollTop()){  fixItem.addClass("fixed");  }
+  if(baseline.offset().top > $(window).scrollTop()){  fixItem.removeClass("fixed");  }
+
+
 });
--- js/kor/sub.js
+++ js/kor/sub.js
@@ -38,3 +38,11 @@ $(document).ready(function(){
 	});
 
 });
+
+$(window).on("load scroll", function(){
+
+	var fixItem = $(".cont-top");
+  var baseline = $(".cont-wrap");
+  if(fixItem.offset().top <= $(window).scrollTop()){  fixItem.addClass("fixed");  }
+  if(baseline.offset().top > $(window).scrollTop()){  fixItem.removeClass("fixed");  }
+});
